2a480fcda107c41c04a1057601578cce563144d2
Cristian <casberry41@gmail.com>
2023-06-14 10:13:25 -0500

    added react and basic homepage

---
+++ flexflow-app/src/homepage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      <header>
+        <nav>
+          <ul className="menu">
+            <li><a href="/">Home</a></li>
+            <li><a href="/workouts">Workouts</a></li>
+            <li><a href="/nutrition">Nutrition</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li className="auth-buttons">
+              <button className="login-button">Login</button>
+              <button className="signup-button">Sign Up</button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <h1>Welcome to the Workout Website</h1>
+        <p>Track your workouts and achieve your fitness goals!</p>
+      </main>
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ flexflow-app/src/workoutpage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function WorkoutsPage() {
+  return (
+    <div>
+      <h1>Workouts Page</h1>
+      
+      {/* Add your workouts content and components */}
+    </div>
+  );
+}
+
+export default WorkoutsPage;
